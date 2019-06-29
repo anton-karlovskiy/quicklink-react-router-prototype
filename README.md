e02a8d291c252262c4a5c8c0472877f432f3ced3
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-06-29 08:55:00 +0300

    added code splitting with dynamic import & tweaked readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-1. Just dropping the Quicklink default code(inside window onload event) into the index.html.
+## Just dropping the Quicklink default code(inside window onload event) into the index.html
 
 In SPA, window-onload-event is just called one time when the website is loaded or only when you refresh the website(F5) (I double checked it by testing).
 
@@ -7,11 +7,14 @@ And when routing, it's not called which means that it does not prefetches in-vie
 So I think we should implement quicklink for it to be called once a new route has completed.
 Just dropping in the one-line code inside window-onload-event might not be effective for all routes but just one which is the loaded page.
 
-2. Just calling quicklink in componentDidMount of root component(App.js in create react app based project) i.e. after the component tree rendered. I think it is the same as the above case.
-According to testing, componentDidMount is called before window-onload-event.
-The effect is componentDidMount is called once when the website is loaded or only when you refresh the websites(F5). Very similar to window-onload-event case.
+## Just calling quicklink in componentDidMount of root component(App.js in create react app based project) i.e. after the component tree rendered.
 
-3. Calling quicklink() once a navigation to a new route has completed i.e. according to SPA quicklink usage in README, we can call quicklink in componentDidUpdate as well as in ComponentDidMount since just one route is rendered at one time and componentDidMount is just called once while componentDidUpdate is called when a new route is rendered.
+I think it is the same as the above case.
+According to testing, componentDidMount is called before window-onload-event.
+The effect is componentDidMount is called once when the website is loaded or only when you refresh the websites(F5). Similar to window-onload-event case.
+
+## Calling quicklink() once a navigation to a new route has completed
+i.e. according to SPA quicklink usage in README, we can call quicklink in componentDidUpdate as well as in ComponentDidMount since just one route is rendered at one time and componentDidMount is just called once while componentDidUpdate is called when a new route is rendered.
 
 
 The workflow from quicklink README is "Detects links within the viewport".
--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import quicklink from 'quicklink/dist/quicklink.mjs';
 
 import Layout from './hoc/Layout';
-import Home from './components/Home';
-import World from './components/World';
-import Tech from './components/Tech';
-import Contact from './components/Contact';
-import Article1 from './components/Article1';
-import Article2 from './components/Article2';
-import Article3 from './components/Article3';
-import Article4 from './components/Article4';
 import './App.css';
+
+const Home = lazy(() => import('./components/Home'));
+const World = lazy(() => import('./components/World'));
+const Tech = lazy(() => import('./components/Tech'));
+const Contact = lazy(() => import('./components/Contact'));
+const Article1 = lazy(() => import('./components/Article1'));
+const Article2 = lazy(() => import('./components/Article2'));
+const Article3 = lazy(() => import('./components/Article3'));
+const Article4 = lazy(() => import('./components/Article4'));
 
 class App extends Component {
   // when app is mounted for the first time
@@ -36,16 +37,18 @@ class App extends Component {
   render () {
     return (
       <Layout>
-        <Switch>
-          <Route exact path="/pages/world" component={World} />
-          <Route exact path="/pages/tech" component={Tech} />
-          <Route exact path="/pages/contact" component={Contact} />
-          <Route exact path="/pages/article1" component={Article1} />
-          <Route exact path="/pages/article2" component={Article2} />
-          <Route exact path="/pages/article3" component={Article3} />
-          <Route exact path="/pages/article4" component={Article4} />
-          <Route exact path="/" component={Home} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/pages/world" component={World} />
+            <Route exact path="/pages/tech" component={Tech} />
+            <Route exact path="/pages/contact" component={Contact} />
+            <Route exact path="/pages/article1" component={Article1} />
+            <Route exact path="/pages/article2" component={Article2} />
+            <Route exact path="/pages/article3" component={Article3} />
+            <Route exact path="/pages/article4" component={Article4} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Suspense>
       </Layout>
     );
   }
