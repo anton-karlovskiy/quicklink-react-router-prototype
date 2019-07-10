ad9752cdab866c6e67c754b57cabddc8678b294e
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-10 13:08:28 +0300

    added webpack magic comments for constant chunk naming

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import quicklink from 'quicklink/dist/quicklink.mjs';
 import Layout from './hoc/Layout';
 import './App.css';
 
-const Home = lazy(() => import('./components/Home'));
-const World = lazy(() => import('./components/World'));
-const Tech = lazy(() => import('./components/Tech'));
-const Contact = lazy(() => import('./components/Contact'));
-const Article1 = lazy(() => import('./components/Article1'));
-const Article2 = lazy(() => import('./components/Article2'));
-const Article3 = lazy(() => import('./components/Article3'));
-const Article4 = lazy(() => import('./components/Article4'));
+// ray test touch <
+const Home = lazy(() => import(/* webpackChunkName: "home" */ './components/Home'));
+const World = lazy(() => import(/* webpackChunkName: "world" */ './components/World'));
+const Tech = lazy(() => import(/* webpackChunkName: "tech" */ './components/Tech'));
+const Contact = lazy(() => import(/* webpackChunkName: "contact" */ './components/Contact'));
+const Article1 = lazy(() => import(/* webpackChunkName: "article1" */ './components/Article1'));
+const Article2 = lazy(() => import(/* webpackChunkName: "article2" */ './components/Article2'));
+const Article3 = lazy(() => import(/* webpackChunkName: "article3" */ './components/Article3'));
+const Article4 = lazy(() => import(/* webpackChunkName: "article4" */ './components/Article4'));
+// ray test touch >
 
 class App extends Component {
   // when app is mounted for the first time
@@ -30,9 +32,40 @@ class App extends Component {
     }
   }
 
+  // ray test touch <
   quicklinkHandler = () => {
-    quicklink();
+    // quicklink();
+    if (process.env.NODE_ENV === 'production') {
+      console.log('ray : production mode quicklink');
+      quicklink({
+        url: [
+          '/static/js/home.4e80f7df.chunk.js',
+          '/static/js/world.4a0125db.chunk.js',
+          '/static/js/tech.63599b3e.chunk.js',
+          '/static/js/contact.6d31ac3e.chunk.js',
+          '/static/js/article1.8993d440.chunk.js',
+          '/static/js/article2.d91704e7.chunk.js',
+          '/static/js/article3.21ad624d.chunk.js',
+          '/static/js/article4.dbd75d2c.chunk.js'
+        ]
+      });
+    } else {
+      console.log('ray : development mode quicklink');
+      quicklink({
+        url: [
+          '/static/js/home.chunk.js',
+          '/static/js/world.chunk.js',
+          '/static/js/tech.chunk.js',
+          '/static/js/contact.chunk.js',
+          '/static/js/article1.chunk.js',
+          '/static/js/article2.chunk.js',
+          '/static/js/article3.chunk.js',
+          '/static/js/article4.chunk.js'
+        ]
+      });
+    }
   };
+  // ray test touch >
 
   render () {
     return (
